6098d90c77a34c76cc9b396e8c5773bae0435a9a
akshat227 <akshatkhare364@gmail.com>
2025-07-03 14:17:24 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <SFML/Audio.hpp>
+#include <iostream>
+int main() {
+  sf::Music music;
+    
+    if (!music.openFromFile("bls.wav")) {
+        return -1; 
+    }
+    music.setLoop(true);
+    music.play();
+    std::cin.get();
+    return 0;
+}
